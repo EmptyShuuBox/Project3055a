a6609081a5716c55f4753f4ca57920e08dbfa770
draco2134435 <ms3990@g.rit.edu>
2016-01-15 14:46:41 -0500

    commit 5
    Added new sprite, added movement script and fixed tile quality.

---
+++ 3055a/Assets/Scripts/Jump.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+    public float jumpHeight = 0.0f;
+    private float movey = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKey(KeyCode.Space))
+        {
+            Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+            velocity.y = CalculateJumpVerticalSpeed(jumpHeight);
+            GetComponent<Rigidbody2D>().velocity = velocity;
+        }
+	}
+    public static float CalculateJumpVerticalSpeed(float targetjumpHeight)
+    {
+        return Mathf.Sqrt(2f * targetjumpHeight * Physics2D.gravity.y);
+    }
+}
---
+++ 3055a/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+      public float speed = 0f;
+      private float movex = 0f;
+      private float movey = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            movex = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            movex = 1;
+        }
+        else
+        {
+            movey = 0;
+            movex = 0;
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * speed, movey * speed);
+    }
+}
