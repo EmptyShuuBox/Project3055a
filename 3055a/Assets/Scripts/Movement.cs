2dbb743f67af84136e9a2c6d928636e27a366796
draco2134435 <ms3990@g.rit.edu>
2016-01-16 01:25:06 -0500

    Commit 6
    Added shooting with rotation(360*) and Jumping.

--- 3055a/Assets/Scripts/Movement.cs
+++ 3055a/Assets/Scripts/Movement.cs
@@ -3,28 +3,66 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
       public float speed = 0f;
+      public float jump = 0f;
       private float movex = 0f;
-      private float movey = 0f;
-	// Use this for initialization
+      
+      bool Grounded = false;
+    // Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // simple left right movement
         if (Input.GetKey(KeyCode.A))
         {
             movex = -1;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movex * speed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().rotation = 0;
+            jumpMechanic();
         }
         else if (Input.GetKey(KeyCode.D))
         {
             movex = 1;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movex * speed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().rotation = 0;
+            jumpMechanic();
         }
+        
         else
         {
-            movey = 0;
+            
             movex = 0;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movex * speed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().rotation = 0;
         }
-        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * speed, movey * speed);
+        jumpMechanic();
+       
+    }
+
+    //jump
+    
+    private void jumpMechanic()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            if (Grounded)
+            {
+
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,
+                                                                   jump);
+            }
+        }
+    }
+    //Check to see if grounded.
+    void OnTriggerEnter2D()
+    {
+        Grounded = true;
+    }
+    void OnTriggerExit2D()
+    {
+        Grounded = false;
     }
 }
---
+++ 3055a/Assets/Scripts/shooting.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shooting : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public GameObject bulletLocation;
+    public float fireDelay = 0;
+    float cooldownTimer = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cooldownTimer -= Time.deltaTime;
+	    if(Input.GetKey(KeyCode.Mouse0) && cooldownTimer<=0)
+            {
+                cooldownTimer = fireDelay;
+                Instantiate(bulletPrefab, bulletLocation.transform.position, transform.rotation);
+            }
+	}
+}
