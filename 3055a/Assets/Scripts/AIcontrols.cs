4d8aae087a63c51bcb7162e26dbe82b31a3f4758
draco2134435 <ms3990@g.rit.edu>
2016-01-20 17:16:00 -0500

    Commit 9
    Ai movement added.

---
+++ 3055a/Assets/Scripts/AIcontrols.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIcontrols : MonoBehaviour
+{
+    public float enemyMoveSpeed = 0f;
+    public float walkDistance = 0f;
+    public bool move = false;
+    private int timer = 0;
+    Vector3 walkAmt;
+    Vector3 defaultPos;
+    private bool flipped = false;
+    // Use this for initialization
+    void Start()
+    {
+        defaultPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        
+        
+        Vector3 currPos = transform.position;
+        float maxPos = defaultPos.x + .5f * walkDistance;
+        float minPos = defaultPos.x - .5f * walkDistance;
+        
+        if (currPos.x < maxPos && currPos.x > minPos)
+        {
+            
+            move = true;
+            if(!flipped)
+            {
+                walkAmt.x = -enemyMoveSpeed * Time.deltaTime;
+                transform.Translate(walkAmt);
+            }
+            else
+            {
+                walkAmt.x = enemyMoveSpeed * Time.deltaTime;
+                transform.Translate(walkAmt);
+            }
+            
+        }
+        else
+        {
+            walkAmt.x = 0;
+            move = false;
+        }
+
+        if (move == true)
+        {
+            timer = 0;
+        }
+        else
+        {
+            timer += 1;
+            
+            if (timer == 100)
+            {
+                print("hello");
+                Vector3 theScale = transform.localScale;
+                theScale.x *= -1;
+                transform.localScale = theScale;
+                if (!flipped)
+                {
+                    walkAmt.x = enemyMoveSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    walkAmt.x = -enemyMoveSpeed * Time.deltaTime;
+                } 
+
+                transform.Translate(walkAmt);
+
+                flipped = !flipped;
+                move = true;
+            }
+        }
+        
+    }
+}
--- 3055a/Assets/Scripts/Switch.cs
+++ 3055a/Assets/Scripts/Switch.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Switch : MonoBehaviour {
-    public bool doorOpen = false;
+    private bool doorOpen = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,25 +10,22 @@ public class Switch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        OnTriggerEnter2D();
+       
 
     }
-    void OnTriggerEnter2D()
+    void OnTriggerStay2D()
     {
+        
         GameObject entrance = GameObject.Find("Door");
         Vector3 entrancePos = entrance.transform.position;
-
+        
         if (Input.GetKey(KeyCode.Q) == true && doorOpen == false)
         {
+         
             entrancePos.y += 1.2f;
             entrance.transform.position = entrancePos;
-            doorOpen = !doorOpen;
+            doorOpen = true;
         }
-        else if (Input.GetKey(KeyCode.Q) == true && doorOpen == true)
-        {
-            entrancePos.y -= 1.2f;
-            entrance.transform.position = entrancePos;
-            doorOpen = !doorOpen;
-        }
+        
     }
 }
