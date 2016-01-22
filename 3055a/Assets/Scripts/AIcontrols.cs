1c7748af7669e3469845ab7b325b32c745e9197f
draco2134435 <ms3990@g.rit.edu>
2016-01-22 14:23:29 -0500

    Commit 10
    Added ai enemy detection, and following.

--- 3055a/Assets/Scripts/AIcontrols.cs
+++ 3055a/Assets/Scripts/AIcontrols.cs
@@ -7,6 +7,7 @@ public class AIcontrols : MonoBehaviour
     public float walkDistance = 0f;
     public bool move = false;
     private int timer = 0;
+    public int rotationSpeed;
     Vector3 walkAmt;
     Vector3 defaultPos;
     private bool flipped = false;
@@ -24,57 +25,85 @@ public class AIcontrols : MonoBehaviour
         Vector3 currPos = transform.position;
         float maxPos = defaultPos.x + .5f * walkDistance;
         float minPos = defaultPos.x - .5f * walkDistance;
-        
-        if (currPos.x < maxPos && currPos.x > minPos)
-        {
-            
-            move = true;
-            if(!flipped)
-            {
-                walkAmt.x = -enemyMoveSpeed * Time.deltaTime;
-                transform.Translate(walkAmt);
-            }
-            else
-            {
-                walkAmt.x = enemyMoveSpeed * Time.deltaTime;
-                transform.Translate(walkAmt);
-            }
-            
-        }
-        else
-        {
-            walkAmt.x = 0;
-            move = false;
-        }
 
-        if (move == true)
-        {
-            timer = 0;
-        }
-        else
-        {
-            timer += 1;
-            
-            if (timer == 100)
-            {
-                print("hello");
-                Vector3 theScale = transform.localScale;
-                theScale.x *= -1;
-                transform.localScale = theScale;
-                if (!flipped)
+        // If the player is not detected
+        if(GetComponentInChildren<detection>().enemyDetected == false)
+           { 
+                if (currPos.x < maxPos && currPos.x > minPos)
                 {
-                    walkAmt.x = enemyMoveSpeed * Time.deltaTime;
+            
+                    move = true;
+                    if(!flipped)
+                    {
+                        walkAmt.x = -enemyMoveSpeed * Time.deltaTime;
+                        transform.Translate(walkAmt);
+                    }
+                    else
+                    {
+                        walkAmt.x = enemyMoveSpeed * Time.deltaTime;
+                        transform.Translate(walkAmt);
+                    }
+            
                 }
                 else
                 {
-                    walkAmt.x = -enemyMoveSpeed * Time.deltaTime;
-                } 
+                    walkAmt.x = 0;
+                    move = false;
+                }
 
+                if (move == true)
+                {
+                    timer = 0;
+                }
+                else
+                {
+                    timer += 1;
+
+                    if (timer == 100)
+                    {
+
+                        Vector3 theScale = transform.localScale;
+                        theScale.x *= -1;
+                        transform.localScale = theScale;
+                        if (!flipped)
+                        {
+                            walkAmt.x = enemyMoveSpeed * Time.deltaTime;
+                        }
+                        else
+                        {
+                            walkAmt.x = -enemyMoveSpeed * Time.deltaTime;
+                        }
+
+                        transform.Translate(walkAmt);
+
+                        flipped = !flipped;
+                        move = true;
+                    }
+                }
+        }
+
+        //if the player is detected
+        else
+        {
+            Transform target = GameObject.FindWithTag("Player").transform;
+
+            if (target != null)
+            {
+                Vector3 dir = target.position - transform.position;
+                // Only needed if objects don't share 'z' value.
+                /*dir.z = 0.0f;
+                if (dir != Vector3.zero)
+                    transform.rotation = Quaternion.Slerp(transform.rotation,
+                        Quaternion.FromToRotation(Vector3.right, dir),
+                        rotationSpeed * Time.deltaTime);
+                        */
+                //Move Towards Target
+                walkAmt.x = (target.position.x - transform.position.x)* 1/(Mathf.Abs(target.position.x - transform.position.x))
+                    * enemyMoveSpeed * Time.deltaTime;
                 transform.Translate(walkAmt);
-
-                flipped = !flipped;
-                move = true;
             }
+            
+
         }
         
     }
---
+++ 3055a/Assets/Scripts/detection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class detection : MonoBehaviour {
+    public GameObject target;
+    public bool enemyDetected = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerStay2D(Collider2D other)
+    {
+        enemyDetected = true;
+        
+    }
+
+}
